399c0b88d8c9d15373e85feb17a6406d24097dc9
zhigongzhang <zhigongzhang@zhigongzhang-PC>
2018-08-02 15:42:18 +0800

    首次创建

--- src/main/java/com/jing/cloud/agent/client/handler/ClientHandler.java
+++ src/main/java/com/jing/cloud/agent/client/handler/ClientHandler.java
@@ -3,8 +3,10 @@ package com.jing.cloud.agent.client.handler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.jing.cloud.module.Authentication;
 import com.jing.cloud.module.Message;
 import com.jing.cloud.module.MessageCode;
+import com.util.RandomUtil;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -15,6 +17,8 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
 
 	private final Logger logger = LogManager.getLogger(ClientHandler.class);
 	
+	private boolean registerStatus = false; //客户端注册状态
+	
 	public ClientHandler() {
 		
 	}
@@ -22,6 +26,16 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
 	@Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		logger.info("收到服务器返回消息:" + msg);
+		Message message = (Message)msg;
+		int type = message.getType();
+		Object data = message.getData();
+		if(type == MessageCode.REGISTER_ERROR) {
+			ctx.close();
+			logger.info("认证失败关闭连接......");
+		}else if(type == MessageCode.REGISTER_SUCCESS) {
+			logger.info("认证成功......");
+			registerStatus = true;
+		}
 	}
 	
 	@Override
@@ -57,7 +71,15 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
 				/* 写超时 */
 				logger.debug("发送心跳");
 				Message msg = new Message();
-				msg.setType(MessageCode.HEARTBEAT);
+				if(registerStatus) {
+					msg.setType(MessageCode.HEARTBEAT);
+				}else {
+					//String token = RandomUtil.GetGuid32();
+					msg.setType(MessageCode.REGISTER);
+					Authentication authInfo = new Authentication("fort", "fort");
+					msg.setData(authInfo);
+					//msg.setToken(token);
+				}
 				ctx.channel().writeAndFlush(msg);
 			} else if (event.state() == IdleState.ALL_IDLE) {
 				/* 总超时 */
