ad4cb9e34cbc55bbbcc27a653ad79611a284f8ca
zhigongzhang <zhigongzhang@zhigongzhang-PC>
2018-08-31 14:22:36 +0800

    更新安装脚本

--- src/main/resources/tar_command.sh
+++ src/main/resources/tar_command.sh
@@ -2,13 +2,13 @@
 tmp=$1
 basepath=$(cd `dirname $0`; pwd)
 mkdir -p ${basepath}/${tmp}
-cp -rf agent ${basepath}/${tmp}/
+cp -rf ${basepath}/agent ${basepath}/${tmp}/
 cd ${basepath}/${tmp}
 echo "server.port=8088" > agent/config/application.properties
 echo "spring.netty.clientId=${2}" >> agent/config/application.properties
 echo "spring.netty.secret=${3}" >> agent/config/application.properties
-echo "spring.oauth.oauthUri=https://192.168.1.206/agent-oauth2/oauth/token" >> agent/config/application.properties
-echo "console.uri=https://192.168.1.206/proxy-console/api/getConnHost" >> agent/config/application.properties
+echo "spring.oauth.oauthUri=https://jing-cloud.com/agent-oauth2/oauth/token" >> agent/config/application.properties
+echo "console.uri=https://jing-cloud.com/proxy-console/api/getConnHost" >> agent/config/application.properties
 echo "server.keystorePath=config/localhost.keystore" >> agent/config/application.properties
 echo "server.keystorePassword=changeit" >> agent/config/application.properties
 echo "logging.config=classpath:log4j2.xml" >> agent/config/application.properties
