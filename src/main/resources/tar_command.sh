e715fb51eb5adc0c0da52b57b79f550ab31727c7
zhigongzhang <zhigongzhang@zhigongzhang-PC>
2018-09-10 12:40:05 +0800

    删除静态域名

--- src/main/resources/tar_command.sh
+++ src/main/resources/tar_command.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 tmp=$1
+baseHost=$4
 basepath=$(cd `dirname $0`; pwd)
 mkdir -p ${basepath}/${tmp}
 cp -rf ${basepath}/agent ${basepath}/${tmp}/
@@ -7,8 +8,8 @@ cd ${basepath}/${tmp}
 echo "server.port=8088" > agent/config/application.properties
 echo "spring.netty.clientId=${2}" >> agent/config/application.properties
 echo "spring.netty.secret=${3}" >> agent/config/application.properties
-echo "spring.oauth.oauthUri=https://jing-cloud.com/agent-oauth2/oauth/token" >> agent/config/application.properties
-echo "console.uri=https://jing-cloud.com/proxy-console/api/getConnHost" >> agent/config/application.properties
+echo "spring.oauth.oauthUri=https://${baseHost}/agent-oauth2/oauth/token" >> agent/config/application.properties
+echo "console.uri=https://${baseHost}/proxy-console/api/getConnHost" >> agent/config/application.properties
 echo "server.keystorePath=config/localhost.keystore" >> agent/config/application.properties
 echo "server.keystorePassword=changeit" >> agent/config/application.properties
 echo "logging.config=classpath:log4j2.xml" >> agent/config/application.properties
